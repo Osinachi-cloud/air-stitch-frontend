d1e0b16d8bf454a257e555e6f19c87f5688e41c5
Uchenna Ogbodo <Uchenna.Ogbodo@Interswitchng.com>
2026-03-21 13:45:17 +0100

    update

--- src/env/URLs.tsx
+++ src/env/URLs.tsx
@@ -1,3 +1,3 @@
-// export const baseUrL = `https://stitch-backend-1.onrender.com/api/v1`;
-export const baseUrL = `http://localhost:8090/api/v1`;
+export const baseUrL = `https://stitch-backend-1.onrender.com/api/v1`;
+// export const baseUrL = `http://localhost:8090/api/v1`;
 export const NEXT_PUBLIC_PAYSTACK_PUBLIC_KEY = 'pk_test_323e5dfdac044ffe641ab7a9c57053d0d17a5c17';
